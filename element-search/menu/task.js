4a13d20e21280eb636da5b2854f7e7994bd46bf5
Xenia Ch <eradonna@yandex.ru>
2023-04-19 22:12:44 +0300

    поправки в домашнее задание номер 2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,14 +1,14 @@
 const menuLinks = document.querySelectorAll(".menu__link");
 let menuArray = Array.from(menuLinks);
 
-for (i = 0; i < menuArray.length; i++) {
+for (let i = 0; i < menuArray.length; i++) {
    let currentMenu = menuArray[i];
    currentMenu.onclick = clickAction;
    
    function clickAction () {      
       let currentMenuHasSubmenu = currentMenu.closest(".menu__item").querySelector(".menu_sub");
       if (currentMenuHasSubmenu != null) {
-      currentMenuHasSubmenu.classList.add("menu_active");
+      currentMenuHasSubmenu.classList.toggle("menu_active");
       return false;
       }      
    }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,29 +2,27 @@ const arrowRight = document.querySelector(".slider__arrow_next");
 const arrowLeft = document.querySelector(".slider__arrow_prev");
 const images = document.getElementsByClassName("slider__item");
 let imageArr = Array.from(images);
-let imageCount = 0;
 
-
-function clickRight () {   
-   if (imageArr[imageCount].className.includes("slider__item_active")) {
-      imageArr[imageCount].classList.remove("slider__item_active");
-      imageCount = imageCount + 1;
-      if (imageCount === imageArr.length) {
-         imageCount = 0;
-      }
-      imageArr[imageCount].classList.add("slider__item_active");        
-   }      
+function clickRight () {
+   let findSlide = (slide) => slide.className.includes("slider__item_active");
+   let imageCount = imageArr.findIndex(findSlide);  
+   imageArr[imageCount].classList.remove("slider__item_active");
+   imageCount = imageCount + 1;
+   if (imageCount === imageArr.length) {
+      imageCount = 0;
+   }
+   imageArr[imageCount].classList.add("slider__item_active");           
 }
 
 function clickLeft () {
-   if (imageArr[imageCount].className.includes("slider__item_active")) {
-      imageArr[imageCount].classList.remove("slider__item_active");
-      imageCount = imageCount - 1;
-      if (imageCount === -1) {
-         imageCount = imageArr.length-1;
-      }
-      imageArr[imageCount].classList.add("slider__item_active");         
-   } 
+   let findSlide = (slide) => slide.className.includes("slider__item_active");
+   let imageCount = imageArr.findIndex(findSlide);  
+   imageArr[imageCount].classList.remove("slider__item_active");
+   imageCount = imageCount - 1;
+   if (imageCount === -1) {
+      imageCount = imageArr.length-1;
+   }
+   imageArr[imageCount].classList.add("slider__item_active");         
 }
 
 arrowRight.onclick = clickRight;
